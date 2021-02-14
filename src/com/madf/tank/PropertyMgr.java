dff2cae545d2cb5febecca0f7e6f6a1966c83b94
madengfeng <1215514252@qq.com>
2021-02-14 22:14:05 +0800

    加入配置文件

---
+++ src/com/madf/tank/PropertyMgr.java
@@ -0,0 +1,30 @@
+package com.madf.tank;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * 配置文件管理类
+ */
+public class PropertyMgr {
+
+    static Properties props = new Properties();
+
+    static {
+        try {
+            props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object get(String key) {
+        if (props == null) return null;
+        return props.get(key);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PropertyMgr.get("initTankCount"));
+    }
+
+}
